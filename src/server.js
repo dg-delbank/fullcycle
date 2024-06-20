a21f5376be064c1f74dea8a1b245b28d3e6909ab
dg-delbank <165733779+dg-delbank@users.noreply.github.com>
2024-06-20 13:43:26 -0300

    changing db

--- src/server.js
+++ src/server.js
@@ -1,31 +1,32 @@
 const express = require('express');
-const sql = require('mssql');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 
-const config = {
-  user: 'sa',
-  password: '123!@hghJGFHGHGjh',
-  server: 'localhost',
-  database: 'db',
-  options: {
-    encrypt: true, 
-  },
-};
+const db = new sqlite3.Database('./sqlite-data/db.sqlite');
 
-app.get('/buscar', async (req, res) => {
-  try {
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS tabela (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT,
+      idade INTEGER
+    )
+  `);
 
-    await sql.connect(config);
+  db.run(`
+    INSERT INTO tabela (nome, idade) VALUES ('João', 30), ('Maria', 25), ('Pedro', 40)
+  `);
+});
 
-    const result = await sql.query`SELECT * FROM tabela`;
-
-    res.json(result.recordset);
-  } catch (err) {
-    console.error('Erro ao buscar conteúdo:', err.message);
-    res.status(500).send('Erro ao buscar conteúdo');
-  } finally {
-    sql.close();
-  }
+app.get('/buscar', (req, res) => {
+  db.all('SELECT * FROM tabela', (err, rows) => {
+    if (err) {
+      console.error('Erro ao buscar conteúdo:', err.message);
+      res.status(500).send('Erro ao buscar conteúdo');
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 const server = app.listen(3000, () => {
